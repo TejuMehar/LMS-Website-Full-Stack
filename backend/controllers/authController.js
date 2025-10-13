5c735bccdc049556dbe772ab4e378d332eb1cf2c
Tejas mehar <tejas mehar7@gmail.com>
2025-10-13 09:44:06 +0530

    Write Login and LogOut Function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -41,3 +41,41 @@ export const signUp = async(req,res)=>{
     return res.status(500).json({message:`SignUp error ${error}`})
    }
 }
+
+
+
+export const login = async(req,res)=>{
+    try{
+      const {email,password} = req.body;
+      let user = await User.findOne({email});
+      if(!user){
+        return res.status(404).json({message: "User not found"})
+      } 
+      let isMatch =await bcrypt.compare(password,user.password);
+      if(!isMatch){
+        return res.status(400).json({message: "Incorrect Password"});
+      }
+       let token = await genToken(user._id);
+      req.cookie("token",token,
+           {
+            httpOnly: true,
+            secure: false,
+            sameSite: "Strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+           }
+      )
+      return res.status(201).json(user);
+    }catch(error){
+        return res.status(5000).json({message: `Login error ${error}`})
+    }
+}
+
+
+export const logOut = async(req,res)=>{
+    try{
+         await res.clearCookie("token");
+         return res.status(200).json({message: `LogOut Successfully`})
+    }catch(error){
+        return res.status(500).json({message: `LogOut Error ${error}`})
+    }
+}
