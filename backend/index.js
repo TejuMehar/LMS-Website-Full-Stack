7c8b4f561384beb1e8fbab82ed9e21d40528404f
Tejas mehar <tejas mehar7@gmail.com>
2025-10-13 07:58:39 +0530

    Created Backend Folder

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello From Tejas Mehar");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
