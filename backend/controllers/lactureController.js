7221e9b04105bbd32a58b7461cf6d0aa2708c4dd
Tejas mehar <tejas mehar7@gmail.com>
2026-01-04 16:01:25 +0530

    Created edit Course and Remove Course

--- backend/controllers/lactureController.js
+++ backend/controllers/lactureController.js
@@ -1,3 +1,4 @@
+import { uploadOnCloudinary } from "../config/cloudinary";
 import Lacture from "../model/lactureModel";
 
 export const createLacture = async (req, res) => {
@@ -20,21 +21,77 @@ export const createLacture = async (req, res) => {
 
     return res.status(201).json({ lacture, course });
   } catch (error) {
-    return res.status(500).json({ message: ` Failed to Create Lacture error ${error}` });
+    return res
+      .status(500)
+      .json({ message: ` Failed to Create Lacture error ${error}` });
   }
 };
 
 
-export const getCourseLactures = async(req,res)=>{
-    try{
-        const {courseId} = req.params;
-        const course = await Course.findById(courseId).populate("lactures");
-        if(!course){
-            return res.status(404).json({message : "Course Not Found"})
-        }
-        await course.save();
-        return res.status(200).json(course);
-    }catch(error){
-        return res.status(500).json({message : `Failed to get Course Lactures ${error}`})
+
+export const getCourseLactures = async (req, res) => {
+  try {
+    const { courseId } = req.params;
+    const course = await Course.findById(courseId).populate("lactures");
+    if (!course) {
+      return res.status(404).json({ message: "Course Not Found" });
     }
-}
+    await course.save();
+    return res.status(200).json(course);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `Failed to get Course Lactures ${error}` });
+  }
+};
+
+
+
+export const editLacture = async (req, res) => {
+  try {
+    const { lactureId } = req.params;
+    const { isPreviewFree, lactureTitle } = req.body();
+    const lacture = await Lacture.findById(lactureId);
+    if (!lacture) {
+      return res.status(404).json({ message: "Lacture Not Found" });
+    }
+
+    let videoUrl;
+
+    if (req.file) {
+      videoUrl = await uploadOnCloudinary(req.file.path);
+      lacture.videoUrl = videoUrl;
+    }
+
+    if (lactureTitle) {
+      lacture.lactureTitle = lactureTitle;
+    }
+    lacture.isPreviewFree = isPreviewFree;
+
+    await lacture.save();
+    return res.status(201).json(lacture);
+  } catch (error) {
+    return res.status(500).json({ message: `Failed to Edit Course ${error}` });
+  }
+};
+
+export const removeLacture = async (req, res) => {
+  try {
+    const { lactureId } = req.params;
+    const lacture = await Lacture.findByIdAndDelete(lactureId);
+
+    if (!lacture) {
+      return res.status(400).json({ message: "Lacture not Found" });
+    }
+    await Course.updateOne(
+      { lactures: lactureId },
+      { $pull: { lcatures: lactureId } }
+    );
+
+    return res.status(200).json({ message: "Lacture Removed" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `Failed to Remove Course ${error}` });
+  }
+};
