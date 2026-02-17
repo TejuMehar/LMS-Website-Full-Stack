10d298327fe4fc702ec8c0502bc2061d202201ed
Tejas mehar <tejas mehar7@gmail.com>
2026-02-17 19:34:37 +0530

    Fixed The Signup error

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,151 +1,211 @@
-import React, { useState } from 'react'
-import logo from "../assets/logo.jpg"
-import google from "../assets/google.jpg"
+import React, { useState } from "react";
+import logo from "../assets/logo.jpg";
+import google from "../assets/google.jpg";
 import { FaRegEyeSlash } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
-import { useNavigate } from "react-router-dom"
-import axios from "axios"
-import { serverUrl } from '../App';
-import { toast } from 'react-toastify';
-import {ClipLoader} from "react-spinners";
-import { useDispatch} from "react-redux";
-import { setUserData} from "../redux/userSlice.js"
-import { signInWithPopup } from 'firebase/auth';
-import { auth, provider } from '../../utils/firebase.js';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { serverUrl } from "../App";
+import { toast } from "react-toastify";
+import { ClipLoader } from "react-spinners";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../redux/userSlice.js";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../utils/firebase.js";
 
 function SignUp() {
-   const [show,setShow] = useState(false);
-   const navigate = useNavigate();
-   const [name,setName] = useState("");
-   const [email,setEmail] = useState("");
-   const [password,setPassword] = useState("");
-   const [role,setRole] = useState("student");
-   const [loading,setLoading] = useState(false);
-   const dispatch = useDispatch()
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("student");
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
 
-   const handleSignup = async()=>{
-       setLoading(true)
-      try{
-        const result = await axios.post(serverUrl + "/api/auth/signup",
-          {name,password,email,role },{withCredentials: true}) 
-          dispatch(setUserData(result.data));
-          setLoading(false)
-          navigate("/")
-          toast.success("Signup Successfully",{
-          position: "top-center",
-          autoClose: 2000,
-          })
-      }catch(error){
-        console.log(error);
-        setLoading(false)
-        toast.error(error.responce.data.message,{
-          position: "top-center",
-          autoClose: 2000,
-          })
-      }
-   }
+  const handleSignup = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.post(
+        serverUrl + "/api/auth/signup",
+        { name, password, email, role },
+        { withCredentials: true },
+      );
+      dispatch(setUserData(result.data));
+      setLoading(false);
+      navigate("/");
+      toast.success("Signup Successfully", {
+        position: "top-center",
+        autoClose: 2000,
+      });
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      toast.error(error.response?.data?.message || "Something went wrong", {
+        position: "top-center",
+        autoClose: 2000,
+      });
+    }
+  };
 
+  const GoogleSignUp = async () => {
+    try {
+      const response = await signInWithPopup(auth, provider);
+      let user = response.user;
+      let name = user.displayName;
+      let email = user.email;
 
-   const GoogleSignUp = async()=>{
-  
-       try{
-         const response = await signInWithPopup(auth,provider);
-          let user = response.user;
-          let name = user.displayName;
-          let email = user.email;
-
-         const result = await axios.post(serverUrl + "/api/auth/googleauth",{name,email,role},{withCredentials:true});
-          dispatch(setUserData(result.data));
-          navigate("/")
-          toast.success("Signup Successfully",{
-          position: "top-center",
-          autoClose: 2000,
-          })
-         
-       }catch(error){
-           toast.error(error.responce.data.message,{
-          position: "top-center",
-          autoClose: 2000,
-          })
-       }
-   }
+      const result = await axios.post(
+        serverUrl + "/api/auth/googleauth",
+        { name, email, role },
+        { withCredentials: true },
+      );
+      dispatch(setUserData(result.data));
+      navigate("/");
+      toast.success("Signup Successfully", {
+        position: "top-center",
+        autoClose: 2000,
+      });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong", {
+        position: "top-center",
+        autoClose: 2000,
+      });
+    }
+  };
 
   return (
-    <div className='bg-[#dddbdb] w-[100vw] h-[100vh] flex items-center justify-center'>
-      <form className='w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex' onSubmit={(e)=>e.preventDefault()}>
-          {/* left div */}
-         <div className='md:w-[50%] w-[100%] h-[100%] flex flex-col items-center justify-center gap-3'>
+    <div className="bg-[#dddbdb] w-[100vw] h-[100vh] flex items-center justify-center">
+      <form
+        className="w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        {/* left div */}
+        <div className="md:w-[50%] w-[100%] h-[100%] flex flex-col items-center justify-center gap-3">
+          <div>
+            <h1 className="font-semibold text-[black] text-2xl">
+              Let's get Started
+            </h1>
+            <h2 className="text-[#999797] text-[18px]">Create Your account</h2>
+          </div>
 
-            <div>
-              <h1 className='font-semibold text-[black] text-2xl'>Let's get Started</h1>
-              <h2 className='text-[#999797] text-[18px]'>Create Your account</h2>
-            </div>
-           
-           <div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3'>
-                <label htmlFor="name" className='font-semibold'>Name</label>
-                <input type="text" id='name' className='border-1 w-[100%] h-[35px]
-                 border-[#e7e6e6] text-[15px] px-[20px]' 
-                 placeholder=' Enter Your name' onChange={(e)=>setName(e.target.value)}/>
-           </div>
-           <div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3'>
-                <label htmlFor="email" className='font-semibold'>Email</label>
-                <input type="text" id='email' className='border-1 w-[100%]
-                 h-[35px] border-[#e7e6e6] text-[15px] px-[20px]'
-                  placeholder=' Enter Your email' onChange={(e)=>setEmail(e.target.value)}/>
-           </div>
-           <div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3 relative'>
-                <label htmlFor="password" className='font-semibold'>Password</label>
-                <input type={show ? "text" : "password"} id='password' className='border-1 w-[100%] 
-                h-[35px] border-[#e7e6e6] text-[15px] px-[20px]'
-                 placeholder=' Enter Your password'onChange={(e)=>setPassword(e.target.value)} />
-                { !show ? <FaRegEyeSlash className='absolute w-[20px] h-[20px] curser-pointer right-[5%] bottom-[10%]' onClick={()=>setShow(prev=>!prev)}/>
-                 :<FaEye className='absolute w-[20px] h-[20px] curser-pointer right-[5%] bottom-[10%]' onClick={()=>setShow(prev=>!prev)}/> }
-           </div>
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3">
+            <label htmlFor="name" className="font-semibold">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="border-1 w-[100%] h-[35px]
+                 border-[#e7e6e6] text-[15px] px-[20px]"
+              placeholder=" Enter Your name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3">
+            <label htmlFor="email" className="font-semibold">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="border-1 w-[100%]
+                 h-[35px] border-[#e7e6e6] text-[15px] px-[20px]"
+              placeholder=" Enter Your email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3 relative">
+            <label htmlFor="password" className="font-semibold">
+              Password
+            </label>
+            <input
+              type={show ? "text" : "password"}
+              id="password"
+              className="border-1 w-[100%] 
+                h-[35px] border-[#e7e6e6] text-[15px] px-[20px]"
+              placeholder=" Enter Your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {!show ? (
+              <FaRegEyeSlash
+                className="absolute w-[20px] h-[20px] curser-pointer right-[5%] bottom-[10%]"
+                onClick={() => setShow((prev) => !prev)}
+              />
+            ) : (
+              <FaEye
+                className="absolute w-[20px] h-[20px] curser-pointer right-[5%] bottom-[10%]"
+                onClick={() => setShow((prev) => !prev)}
+              />
+            )}
+          </div>
 
-           <div className="flex md:w-[50%] w-[70%] items-center justify-between gap-4">
-             <span
+          <div className="flex md:w-[50%] w-[70%] items-center justify-between gap-4">
+            <span
               className={`px-[10px] py-[5px] border-[1px] rounded-2xl cursor-pointer hover:border-black ${
-              role === "student" ? "border-black" : "border-[#e7e6e6]"
+                role === "student" ? "border-black" : "border-[#e7e6e6]"
               }`}
               onClick={() => setRole("student")}
-            >Student</span>
+            >
+              Student
+            </span>
 
-              <span className={`px-[10px] py-[5px] border-[1px] rounded-2xl cursor-pointer hover:border-black ${
-               role === "educator" ? "border-black" : "border-[#e7e6e6]"
-               }`}
-                onClick={() => setRole("educator")}
-               >Educator</span>
-              </div>
+            <span
+              className={`px-[10px] py-[5px] border-[1px] rounded-2xl cursor-pointer hover:border-black ${
+                role === "educator" ? "border-black" : "border-[#e7e6e6]"
+              }`}
+              onClick={() => setRole("educator")}
+            >
+              Educator
+            </span>
+          </div>
 
-
-            <button className='w-[80%] h-[40px] bg-black text-white 
+          <button
+            className="w-[80%] h-[40px] bg-black text-white 
             couser-pointer flex items-center 
-            justify-center rounded-[5px]' onClick={handleSignup} disabled={loading}>{loading ? <ClipLoader size={30} color='white'/>: "SignUp"}</button>
-            
-            <div className='w-[80%] flex items-center gap-2'>
-                <div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'></div>
-                <div className='w-[50%] text-[15px] text-[#6f6f6f] flex text-center justify-center'>Or continue</div>
-                <div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'></div>
-            </div>
-           
-           <div className='w-[80%] h-[40px] border-1 border-[black] rounded-[5px] flex items-center justify-center' onClick={GoogleSignUp}>
-                 <img src={google} alt="google" className='w-[25px] '/>
-                 <span className='text-[18px] text-grat-500'>oogle</span>
-           </div>
-            
-            <div className='text-[#6f6f6f]'>an Already have a Account ?
-              <span className='underline underline-offset-1 text-black' onClick={()=>navigate("/login")}>Login</span>
-            </div>
-         </div>
-         {/* right div */}
-        <div className='md:w-[50%] h-full rounded-r-2xl bg-black md:flex items-center justify-center flex-col hidden'>
-           <img src={logo} alt="logo" className='w-[120px] shadow-2xl mb-4'/>
-           <span className='text-2xl text-white font-semibold'>Virtual Courses</span>
-        </div>
+            justify-center rounded-[5px]"
+            onClick={handleSignup}
+            disabled={loading}
+          >
+            {loading ? <ClipLoader size={30} color="white" /> : "SignUp"}
+          </button>
 
+          <div className="w-[80%] flex items-center gap-2">
+            <div className="w-[25%] h-[0.5px] bg-[#c4c4c4]"></div>
+            <div className="w-[50%] text-[15px] text-[#6f6f6f] flex text-center justify-center">
+              Or continue
+            </div>
+            <div className="w-[25%] h-[0.5px] bg-[#c4c4c4]"></div>
+          </div>
+
+          <div
+            className="w-[80%] h-[40px] border-1 border-[black] rounded-[5px] flex items-center justify-center"
+            onClick={GoogleSignUp}
+          >
+            <img src={google} alt="google" className="w-[25px] " />
+            <span className="text-[18px] text-grat-500">oogle</span>
+          </div>
+
+          <div className="text-[#6f6f6f]">
+            an Already have a Account ?
+            <span
+              className="underline underline-offset-1 text-black"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </span>
+          </div>
+        </div>
+        {/* right div */}
+        <div className="md:w-[50%] h-full rounded-r-2xl bg-black md:flex items-center justify-center flex-col hidden">
+          <img src={logo} alt="logo" className="w-[120px] shadow-2xl mb-4" />
+          <span className="text-2xl text-white font-semibold">
+            Virtual Courses
+          </span>
+        </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
